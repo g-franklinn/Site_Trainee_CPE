1cbfdf16439eed99ad7ca0b3e7d70bb05d0793d7
Joao Monteiro <joaosilva@cpejr.com.br>
2023-10-26 11:42:11 -0300

    Desenvolvimento Header_1

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,17 @@ function App() {
     <>
       <div className="header">
 
+        Doti Musical
+
+        <div className="header-menu">
+          OIIII
+        </div>
       </div>
+
       <div className="page-content">
           <h1>Hello World</h1>
       </div>
+
       <div className="footer">
 
       </div>
